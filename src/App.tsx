94d8c44e171be181745e8d031ff5559ad6eed1ef
stefaleon <stefaleon@gmail.com>
2022-11-22 20:40:45 +0200

    testing

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import CryptoJS from "crypto-js";
+import { isEqual, isNull } from "lodash";
+import axios from "axios";
+
+type TypicodeUser = {
+  id: number;
+  name: string;
+};
+
+const App: React.FC = () => {
+  const [users, setUsers] = useState<TypicodeUser[] | null>(null);
+  const [haveData, setHaveData] = useState<boolean>(false);
+
+  console.log("test env", process.env.REACT_APP_TEST_VALUE);
+
+  const encryption_Key = process.env.REACT_APP_EncryptionKey || "";
+  const encrypt = (str: string) => CryptoJS.AES.encrypt(str, encryption_Key).toString();
+
+  const testStr = "abcd";
+  console.log("encrypted test string", encrypt(testStr));
+
+  const fetchUsers = async () => {
+    try {
+      const res = await axios.get(`${process.env.REACT_APP_TYPICODE_API_USERS_ENDPOINT}`);
+      console.log(res.data);
+      if (!isNull(res.data)) setHaveData(true);
+      if (!isEqual(users, res.data)) setUsers(res.data);
+    } catch (error) {
+      return error;
+    }
+  };
+
+  fetchUsers();
+
+  return (
+    <div className="App">
+      App
+      <div>Some test env value: {process.env.REACT_APP_TEST_VALUE}</div>
+      <div>Have data: {haveData.toString()}</div>
+      {!isNull(users) && (
+        <div>
+          Users:{" "}
+          {users.map((item) => (
+            <div key={item.id}>
+              {item.id} {item.name}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
